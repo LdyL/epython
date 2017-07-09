361214f4a6faa9a21f4de64332181a6ebd02ae55
Dongyu Liang <steve.d.leong@gmail.com>
2017-07-09 21:50:10 +0100

    Moved check point of Epiphany[debugging 15]

--- device/device-functions.c
+++ device/device-functions.c
@@ -782,6 +782,7 @@ static struct value_defn recvDataFromDeviceCore(int source) {
  * blocks on receive and then blocks on the initial send to form one overall block
  */
 static struct value_defn sendRecvData(struct value_defn to_send, int target) {
+  raiseError(ERR_CHECK_POINT);
 	if (to_send.type == STRING_TYPE) raiseError(ERR_ONLY_SEND_INT_AND_REAL);
 	if (isLocal(target)) {
 		return sendRecvDataWithDeviceCore(to_send, target-getLargestCoreId(target)*sharedData->nodeId);
@@ -791,7 +792,6 @@ static struct value_defn sendRecvData(struct value_defn to_send, int target) {
 }
 
 static struct value_defn sendRecvDataWithHostProcess(struct value_defn to_send, int hostProcessTarget) {
-  raiseError(ERR_CHECK_POINT);
 	struct value_defn receivedData;
 	cpy(sharedData->core_ctrl[myId].data, &hostProcessTarget, 4);
 	sharedData->core_ctrl[myId].data[5]=to_send.type;
