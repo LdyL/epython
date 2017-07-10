f09836fe6c6d79c2b3191035c88be0490c33fb61
Dongyu Liang <steve.d.leong@gmail.com>
2017-07-10 20:45:24 +0100

    Fixed test code for remote message passing

--- testPyCode/rm_p2p_msgPassing.py
+++ testPyCode/rm_p2p_msgPassing.py
@@ -7,7 +7,7 @@ from parallel import *
 if coreid()==0:
   val=20.0
   print "[core 0]sending value "+str(val)+" to core 17"
-  send(20, val)
+  send(val, 17)
 if coreid()==17:
   print "[core 17]Got value "+str(recv(0))+" form core 0"
 if coreid()==16:
@@ -15,4 +15,4 @@ if coreid()==16:
   print "[core 16]sending value "+str(val)+" to core 2"
   send(val, 2)
 if coreid()==2:
-  print "[core 2]Got value "+str(recv(0))+" form core 0"
+  print "[core 2]Got value "+str(recv(16))+" form core 16"
