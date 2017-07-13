750b202e85e46ac797a69a862986bfd0a8ea5325
Dongyu Liang <steve.d.leong@gmail.com>
2017-07-13 21:45:42 +0100

    Trying to fix bus error[debugging 26.5]

--- host/device-support.c
+++ host/device-support.c
@@ -163,7 +163,7 @@ void monitorCores(struct shared_basic * basicState, struct interpreterconfigurat
 
 	while (totalActive > 0) {
 		for (i=0;i<TOTAL_CORES;i++) {
-			printf("[node %d]core %d[%d]\n", basicState->nodeId,i,active[i]);
+			printf("core %d[%d]\n", i, active[i]);
 			if (active[i]) {
 				checkStatusFlagsOfCore(basicState, configuration, i, requests, commStatus, Parallella_postbox);
 			}
