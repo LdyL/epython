1c3ffe0b548d9238c001531fe39477bca9225116
Dongyu Liang <steve.d.leong@gmail.com>
2017-08-13 15:19:43 +0100

    Updated Python code for test

--- testPyCode/rm_p2p_Sendrecv.py
+++ testPyCode/rm_p2p_Sendrecv.py
@@ -4,17 +4,23 @@ Illustration of P2P blocking sendrecv between core 0 to core 17.
 
 from parallel import *
 
-data=[0]*2
+n=1
 
+data=[0]*2
+i=0
 if coreid()==0:
     data[0]=0.0
     data[1]=0.0
     print "Core"+str(coreid())+" has value: "+"["+str(data[0])+","+str(data[1])+"]"
-    data[1]=sendrecv(data[0],17)
+    while i<n:
+        data[1]=sendrecv(data[1],17)
+        i+=1
     print "Core"+str(coreid())+" has value: "+"["+str(data[0])+","+str(data[1])+"]"
 if coreid()==17:
     data[0]=1.0
     data[1]=1.0
     print "Core"+str(coreid())+" has value: "+"["+str(data[0])+","+str(data[1])+"]"
-    data[1]=sendrecv(data[0],0)
+    while i<n:
+        data[1]=sendrecv(data[1],0)
+        i+=1
     print "Core"+str(coreid())+" has value: "+"["+str(data[0])+","+str(data[1])+"]"
--- testPyCode/rm_p2p_msgPassing.py
+++ testPyCode/rm_p2p_msgPassing.py
@@ -5,15 +5,30 @@ and from core 17 to core 2.
 
 from parallel import *
 
+n=1
+
+i=0
 if coreid()==1:
   val=20
   print "[core 1]sending value "+str(val)+" to core 18"
-  send(val, 18)
+  while i<n:
+    send(val, 18)
+    i+=1
 if coreid()==18:
-  print "[core 18]Got value "+str(recv(1))+" from core 1"
+  val=0
+  while i<n:
+    val=recv(1)
+    i+=1
+  print "[core 18]Got value "+str(val)+" from core 1"
 if coreid()==17:
   val=30.0
   print "[core 17]sending value "+str(val)+" to core 2"
-  send(val, 2)
+  while i<n:
+    send(val, 2)
+    i+=1
 if coreid()==2:
-  print "[core 2]Got value "+str(recv(17))+" from core 17"
+  val=0
+  while i<n:
+    val=recv(17)
+    i+=1
+  print "[core 2]Got value "+str(val)+" from core 17"
